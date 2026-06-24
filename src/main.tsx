a7112912ff48efb61150f8d07f8511db12dc1aac
princefreto <princefreto@gmail.com>
2026-06-24 13:06:19 +0000

    1,

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { Capacitor } from "@capacitor/core";
+
+const isCapacitor = Capacitor.isNativePlatform();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    {isCapacitor ? (
+      <HashRouter>
+        <App />
+      </HashRouter>
+    ) : (
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )}
   </StrictMode>
 );
